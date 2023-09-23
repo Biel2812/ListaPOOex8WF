131e7462a602f2a2144024d788d15c46d8cca811
Biel2812 <130082662+Biel2812@users.noreply.github.com>
2023-09-23 17:48:15 -0300

    Add files via upload

---
+++ PJ007/PJ007/Valorxy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJ007
+{
+    internal class Valorxy
+    {
+        //atributos
+        private double x;
+        private double y;
+        private double valores;
+
+
+
+
+        //metodos 
+        public void setXnumero(double p)
+        {
+            x = p;
+        }
+
+        public void setYnumero(double p)
+        {
+            y = p;
+        }
+
+
+        public double getXnumero()
+        {
+            return x;
+        }
+
+        public double getYnumero()
+        {
+            return y;
+        }
+
+        public double getValores()
+        {
+            return valores;
+        }
+
+        public void calcularValores()
+        {
+            if (x >= y)
+            {
+                Console.WriteLine("O maior  número é: " + x);
+            }
+            else
+            {
+                Console.WriteLine("O maior  número é: " + y);
+
+            }
+
+        }
+    }
+}
+
